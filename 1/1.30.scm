af2f24c8662b583b63880f44cc79660830965c7e
Chris Shea <chris@tie-rack.org>
2009-01-03 10:50:41 -0700

    Exercise 1.30

---
+++ 1/1.30.scm
@@ -0,0 +1,9 @@
+;; Iterative version of sum.
+
+(define (sum term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (+ result (term a)))))
+  (iter a 0))
+
