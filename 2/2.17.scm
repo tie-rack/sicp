82de8589d2cce92cdeab010de83039d79e1ca954
Chris Shea <chris@tie-rack.org>
2009-01-27 19:37:33 -0700

    Exercise 2.17

---
+++ 2/2.17.scm
@@ -0,0 +1,10 @@
+;; Define a procedure last-pair that returns the list that contains
+;; only the last element of a given (nonempty) list:
+
+;; (last-pair (list 23 72 149 34))
+;; (34)
+
+(define (last-pair items)
+  (if (null? (cdr items))
+      items
+      (last-pair (cdr items))))
