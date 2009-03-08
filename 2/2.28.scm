c84812e6776fb163ebce4c833772d5a48f41bfee
Chris Shea <chris@tie-rack.org>
2009-03-08 13:36:07 -0600

    Exercise 2.28 - fringe (aka flatten)

---
+++ 2/2.28.scm
@@ -0,0 +1,20 @@
+;; Write a procedure fringe that takes as argument a tree (represented
+;; as a list) and returns a list whose elements are all the leaves of
+;; the tree arranged in left-to-right order. For example,
+
+;; (define x (list (list 1 2) (list 3 4)))
+
+;; (fringe x)
+;; (1 2 3 4)
+
+;; (fringe (list x x))
+;; (1 2 3 4 1 2 3 4)
+
+(define (fringe items)
+  (define (iter items fringed-items)
+    (cond ((null? items) fringed-items)
+          ((not (pair? items)) (cons items fringed-items))
+          (else (iter 
+                 (cdr items)
+                 (append (reverse (fringe (car items))) fringed-items)))))
+  (reverse (iter items '())))
